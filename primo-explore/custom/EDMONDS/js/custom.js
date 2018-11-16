68a4809b52792d77710625433fb87ab7da93fe65
Dan M <dmoore@users.noreply.github.com>
2018-11-15 17:13:12 -0800

    final tweaks to new search page; fixed logo

--- primo-explore/custom/EDMONDS/js/custom.js
+++ primo-explore/custom/EDMONDS/js/custom.js
@@ -101,21 +101,6 @@ app.value('searchTargets', [{
   }
 }]);
 
-// Add Clickable Logo
-app.controller('prmLogoAfterController', [function () {
-  var vm = this;
-  vm.getIconLink = getIconLink;
-  function getIconLink() {
-    return vm.parentCtrl.iconLink;
-  }
-}]);
-
-app.component('prmLogoAfter', {
-  bindings: { parentCtrl: '<' },
-  controller: 'prmLogoAfterController',
-  template: '<div class="product-logo product-logo-local" layout="row" layout-align="start center" layout-fill id="banner" tabindex="0" role="banner">' + '<a href="http://www.edcc.edu/library/">' + '<img class="logo-image" alt="{{::(&apos;nui.header.LogoAlt&apos; | translate)}}" ng-src="{{$ctrl.getIconLink()}}"/></a></div>'
-});
-
 // Add chat widget to header 
 app.component('prmSearchBookmarkFilterAfter', {
   bindings: {},
