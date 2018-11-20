d10c8f5c14e8fecd775ea3f54a5b77898a7031c0
Dan M <dmoore@users.noreply.github.com>
2018-11-20 13:51:42 -0800

    Added new 'hide language' fix

--- primo-explore/custom/EDMONDS/js/custom.js
+++ primo-explore/custom/EDMONDS/js/custom.js
@@ -133,11 +133,21 @@ app.value('searchTargets', [{
 
 app.component('prmAdvancedSearchAfter', {
   bindings: { parentCtrl: '<' },
-  controller: function controller($scope) {
-    var vm = this;
+  controller: 'prmAdvancedSearchAfterController'
+});
 
-    delete vm.parentCtrl.language;
-  }
+app.controller('prmAdvancedSearchAfterController', function ($scope, $http) {
+  var vm = this;
+  delete vm.parentCtrl.language;
+  setInterval(function () {
+    var scopeElement = document.getElementsByClassName('advanced-search-tabs padded-container layout-wrap layout-align-space-between-center layout-row')[0];
+    if (scopeElement) {
+      scopeElement.addEventListener("click", function () {
+        document.getElementsByClassName('md-primary md-primoExplore-theme md-checked')[0].click();
+      });
+      delete vm.parentCtrl.language;
+    }
+  }, 1);
 });
 // Add chat widget to header 
 app.component('prmSearchBookmarkFilterAfter', {
--- primo-explore/custom/EDMONDS/js/hidelanguage.js
+++ primo-explore/custom/EDMONDS/js/hidelanguage.js
@@ -1,9 +1,16 @@
 app.component('prmAdvancedSearchAfter', {
     bindings: { parentCtrl: '<'},
-               controller: function($scope){
-                    var vm = this;
-               
-        delete vm.parentCtrl.language;
-               
-               }
+        controller: 'prmAdvancedSearchAfterController'
 });
+
+app.controller('prmAdvancedSearchAfterController', function ($scope, $http) {
+    var vm = this;
+    delete vm.parentCtrl.language;
+        setInterval(function(){
+            var scopeElement = document.getElementsByClassName('advanced-search-tabs padded-container layout-wrap layout-align-space-between-center layout-row')[0];
+            if (scopeElement){
+                scopeElement.addEventListener("click", function(){document.getElementsByClassName('md-primary md-primoExplore-theme md-checked')[0].click(); });
+                delete vm.parentCtrl.language;
+            }
+        }, 1)
+});     
